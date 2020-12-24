e746d44a4b6b47db95127d0d7d7b8d49958b666b
aes89 <adrienne.e.smith@outlook.com>
2020-12-24 13:13:16 +1100

    Fridge Component Adds Ingredients Into Array Duplicated ingredients are not added

--- src/components/ingredientAutocomplete/index.js
+++ src/components/ingredientAutocomplete/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
 import { Autocomplete } from "@material-ui/lab";
 import ingredients from "../../data/ingredients.json";
-import { useFormik } from "formik";
+import { Field, Formik, useFormik } from "formik";
 
 export default function AutocompleteIngredients() {
   const formik = useFormik({
@@ -11,35 +11,45 @@ export default function AutocompleteIngredients() {
     },
     //will need to validate?
   });
-  const [value, setValue] = useState(null);
-  useEffect(() => {
-    console.log("useEffect", value);
-  });
+  const [selectedIngredient, setSelectedIngredient] = useState(null);
+  const [values, setValues] = useState([]);
+  // useEffect(() => {
+  const handleClick = () => {
+    if (!values.includes(selectedIngredient)) {
+      setValues([...values, selectedIngredient]);
+    }
+  };
+  console.log("values", values);
   //useEffect, when value not null/empty then add to local
-  console.log("value", value);
 
   return (
-    <div style={{ width: 300 }}>
-      <form onSubmit={formik.handleSubmit}>
-        <Autocomplete
-          ingredientAutocomplete
-          id="ingredient-autocomplete"
-          disableClearable
-          options={ingredients.map((option) => option.name)}
-          renderInput={(params) => {
-            setValue(params.inputProps.value);
-            return (
-              <TextField
-                {...params}
-                label="Add Ingredient"
-                margin="normal"
-                variant="outlined"
-                InputProps={{ ...params.InputProps, type: "search" }}
-              />
-            );
-          }}
-        />
-      </form>
+    <div style={{ marginLeft: "100px", marginTop: "100px", width: 300 }}>
+      <Autocomplete
+        ingredientAutocomplete
+        id="ingredient-autocomplete"
+        disableClearable
+        options={ingredients.map((option) => option.name)}
+        renderInput={(params) => {
+          console.log("params", params.inputProps.value);
+          setSelectedIngredient(params.inputProps.value);
+          return (
+            <TextField
+              {...params}
+              label="Add Ingredient"
+              margin="normal"
+              variant="outlined"
+              InputProps={{ ...params.InputProps, type: "search" }}
+            />
+          );
+        }}
+      />
+      <button onClick={handleClick}>Add</button>
+      {/* <ul>
+      {value.map((value) => (
+        <li key={value}>{value}</li>
+      ))}
+      ;
+    </ul> */}
     </div>
   );
 }
