26db96aed3a3d39bc984748882b66d1a558dccf4
aes89 <adrienne.e.smith@outlook.com>
2020-12-24 11:14:52 +1100

    Autocomplete Logging Value

---
+++ src/components/fridge/addIngredient.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AutocompleteIngredients from "../ingredientAutocomplete/index";
+
+const fridgeContents = [];
+
+const AddIngredient = () => {
+  return (
+    <div>
+      <AutocompleteIngredients />
+    </div>
+  );
+};
+
+export default AddIngredient;
--- src/components/fridge/index.js
+++ src/components/fridge/index.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { useFormik } from "formik";
 import styles from "../styles/loginSignup.module.css";
-import AutocompleteIngredients from "../ingredientAutocomplete";
+import AddIngredient from "./addIngredient";
 
 const Fridge = () => {
   return (
     <div>
       It's a fridge!
-      <AutocompleteIngredients />
+      <AddIngredient />
     </div>
   );
 };
 
 export default Fridge;
+
+// import React from 'react'
+// import BlogPost from "./BlogPost"
+
+// const BlogPosts = ({postData}) => {
+//     return (
+//         <div>
+//             {postData.sort((a,b) => b.modified_date - a.modified_date).map((post) => <BlogPost key={post._id} post={post} />)}
+//         </div>
+//     )
+// }
+
+// export default BlogPosts
