393c5234b07becc0e27afe8041552629f686757a
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2021-01-07 20:35:14 +1000

    added json file with random recipe jokes, can add more later, and input on home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,21 +1,29 @@
 import { connect } from "react-redux";
-import React from "react";
+import React, { useState,useEffect } from "react";
 
 
 import SearchRecipeButton from "../searchButton";
 import styles from "./home.module.css";
 import Logo from "../logo";
+import jokes from "../../data/recipeJokes.json";
 
 const Home = ({ actions, userLoggedIn }) => {
-
+  const [joke, setJoke] = useState("");
   const { setModalOpen } = actions;
+
+  useEffect(() => {
+       let randomObject = jokes[Math.floor(Math.random() * jokes.length)];
+       setJoke(randomObject.name)
+       console.log(randomObject.name)
+       
+      },[])
 
   return (
     <div className={styles.homeLayoutOnly}>
       <Logo />
       <main className={` ${styles.homeContent}`}>
         <div className={styles.homeBox}>
-          <p>Random Food Jokes API</p>
+           <p>{joke}</p>
           <h1>What is in your fridge?</h1>
           <h3>
             Just add your ingredients and FridgeMate will help find recipes
