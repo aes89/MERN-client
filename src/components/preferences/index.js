e34e61c4685225a24ca165abbcb52a7e781ea06d
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2021-01-07 11:43:16 +1000

    route working as it should, need to adjust for displaying current values from redux/local

--- src/components/preferences/index.js
+++ src/components/preferences/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import ReactDOM from "react-dom";
+import { useHistory } from "react-router-dom";
 import { Formik, Field, Form, useFormik } from "formik";
-import preferencesList from "./list";
+import {preferencesList, preferencesName } from "./list";
 import getUserPreferences from "../../utils/get-user-preferences";
 import {
   getPreference,
@@ -44,24 +45,19 @@ const validate = (values) => {
 // actions: which is submit (to db) and get payload/data from db.
 const Preferences = ({ actions, userPreferences, userLoggedIn }) => {
   const classes = useStyles();
+   let history = useHistory();
   const [checked, setChecked] = useState(null);
 
-  //change this to get user preferences from DB
-  useEffect(() => {
-    actions.updatePreferences(getUserPreferences());
-  }, []);
 
   // On page load- This is calling the DB get request to get the initial user preference data
   useEffect(() => {
     getPreference(getUsername())
       .then((pref) => {
-        console.log(pref)
         setPref({ ...pref })
-        actions.updatePreferences({ ...pref });
-        console.log("test", JSON.parse(getPref()))
-      })
-      .then(() => {
-        console.log(userPreferences);
+        actions.updatePreferences(JSON.parse(getPref()));
+        setChecked(JSON.parse(getPref()))
+        console.log("check local", JSON.parse(getPref()))
+        console.log("check redux", userPreferences)
       })
       .catch((error) => {
         console.log("errors");
@@ -73,12 +69,11 @@ const Preferences = ({ actions, userPreferences, userLoggedIn }) => {
             "There may be a problem with the server. Please try again after a few moments."
           );
       });
+     
+
   }, []);
 
-  function changeHandler () {
-    let newState = [...checked, 'Hi buddy'];
-     setChecked(newState);
-  }
+  let usedValues = getPref()
 
   const formik = useFormik({
     //calls boolean validation
@@ -90,12 +85,14 @@ const Preferences = ({ actions, userPreferences, userLoggedIn }) => {
           updatePreference({ ...values }, getUsername())
             .then((pref) => {
               console.log(pref);
+              setPref({ ...pref })
               actions.updatePreferences({ ...pref });
+              console.log("test returned", JSON.parse(getPref()))
+              history.push("/preferences/"+getUsername())
               toast.success("Preferences Updated!")
+               
             })
             .catch((error) => {
-              //console.log("errors")
-              console.log(error.response)
               toast.error("Oh no, error!")
               if (error.response && error.response.status === 404)
                 formik.setStatus("Error getting pref information ");
@@ -145,9 +142,10 @@ const Preferences = ({ actions, userPreferences, userLoggedIn }) => {
                               <label key={index}>
                                 <Field  type="checkbox" name={preference}/>
                                 {preference}
+
                               </label>
                      
-                      ))}
+                       ))}
                      
                       <Button
                         class={styles.updateButton}
--- src/reducers/preferences/index.js
+++ src/reducers/preferences/index.js
@@ -16,13 +16,6 @@ const initialState = { vegetarian: true,
 const userPreferences = createReducer(initialState, (builder) => {
   builder.addCase(updatePreferences, (state, action) => {
     state.preferences = action.payload;
-    // state.preferences.vegan = action.payload.vegan;
-    // state.preferences.glutenFree = action.payload.glutenFree;
-    // state.preferences.dairyFree = action.payload.dairyFree;
-    // state.preferences.veryHealthy = action.payload.veryHealthy;
-    // state.preferences.cheap = action.payload.cheap;
-    // state.preferences.veryPopular = action.payload.veryPopular;
-    // state.preferences.sustainable = action.payload.sustainable;
     console.log("updated pref")
     console.log(action.payload)
   });
