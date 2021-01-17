61090d508083333fe2b314c46c923ec0989af539
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2021-01-17 19:19:49 +1000

    Adjusted config

--- src/config/api.js
+++ src/config/api.js
@@ -10,7 +10,9 @@ export default axios.create({
   baseURL: (production ? "https://fridge-mate.herokuapp.com/" : "http://localhost:3009"), 
   timeout: 10000,
   withCredentials: true,
-  // headers: {
-  //   "access-control-allow-origin": "*",
-  // },
+  headers: {
+    "access-control-allow-origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    "Access-Control-Allow-Headers": 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  },
 });
